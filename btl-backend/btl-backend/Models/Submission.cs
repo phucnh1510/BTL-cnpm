5322d990498b09da02c4e0ac5168d1bc0a0ac63e
Hong Phuc Nguyen <tomnguyen1510.4@gmail.com>
2024-10-04 11:59:34 +0700

    BTL-36 implement submission class

---
+++ btl-backend/btl-backend/Models/Submission.cs
@@ -0,0 +1,26 @@
+﻿namespace btl_backend.Models;
+
+public class Submission
+{
+    public string? SubmissionID { get; set; }
+
+    public string? ProblemID { get; set; }
+
+    public string? UserID { get; set; }
+
+    public string? Status { get; set; }
+
+    public string? Language { get; set; }
+
+    public string? Code { get; set; }
+
+    public int SubmissionCount { get; set; }
+
+    public DateTime SubmissionTime { get; set; }
+
+    public decimal ExecutionTime { get; set; }
+
+    public decimal Memory { get; set; }
+
+    public string? Note { get; set; }
+}
