c140a92378c6bd15867f705a10e03272e7c0b53d
Hong Phuc Nguyen <tomnguyen1510.4@gmail.com>
2024-10-04 11:59:16 +0700

    BTL-36 implement problem class

---
+++ btl-backend/btl-backend/Models/Problem.cs
@@ -0,0 +1,26 @@
+﻿namespace btl_backend.Models;
+
+public class Problem
+{
+    public string? ProblemID { get; set; }
+
+    public string? ProblemName { get; set; }
+
+    public string? ProblemContent { get; set; }
+
+    public Topic? ProblemTopics { get; set; }
+}
+
+public class Topic
+{
+    public List<TopicDetail>? TopicsDetails { get; set; }
+}
+
+public class TopicDetail
+{
+    public string? TopicID { get; set; }
+
+    public string? TopicName { get; set; }
+
+    public string? TopicContent { get; set; }
+}
