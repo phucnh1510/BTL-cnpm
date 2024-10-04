5872c9d9633810c4621425d0136a679f443f6ff5
Hai Ninh <haininh320@gmail.com>
2024-10-04 21:42:48 +0700

    BTL-28-Lmao1

---
+++ ninh/src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let href = "#";
+    export const alt = "LeetCode";
+</script>
+  
+<div class="footer">
+
+    <div class="footer-container">
+        <div class="footer-left">
+            <p>Copyright © 2024 LeetCode</p>
+            <a {href}>Help Center</a> 
+            <a {href}>Jobs</a> 
+            <a {href}>Bug Bounty</a> 
+            <a {href}>Assessment</a> 
+            <a {href}>Students</a> 
+            <a {href}>Terms</a> 
+            <a {href}>Privacy Policy</a>
+        </div>
+    </div>     
+ </div>
