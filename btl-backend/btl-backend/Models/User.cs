571f038b532a4885767257d00adb6195b6914117
Hong Phuc Nguyen <tomnguyen1510.4@gmail.com>
2024-10-04 11:57:18 +0700

    BTL-36 Implement user orm

---
+++ btl-backend/btl-backend/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace btl_backend.Models;
+
+public class User
+{
+    public string? UserID { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? ClassID { get; set; }
+
+    public int Role { get; set; }
+
+    public int Completed { get; set; }
+
+}
