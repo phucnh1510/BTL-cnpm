f90898ac52c0c6b2be7f49b2ed2099f964b6495e
Hong Phuc Nguyen <tomnguyen1510.4@gmail.com>
2024-10-10 20:15:40 +0700

    BTL-42 Implement CodeJudgeService to support code execution and result retrieving for Python and C++

---
+++ btl-backend/btl-backend/Services/CodeJudgeService.cs
@@ -0,0 +1,92 @@
+﻿using System.Diagnostics;
+using btl_backend.Data;
+using btl_backend.Dtos;
+using btl_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace btl_backend.Services;
+
+// Services/CodeJudgeService.cs
+public class CodeJudgeService
+{
+    private class CodeExecutor
+    {
+        public string ExecutePythonCode(string code)
+        {
+            string tempFile = Path.GetTempFileName() + ".py";
+            File.WriteAllText(tempFile, code);
+
+            return ExecuteCode("python", tempFile);
+        }
+
+        public static string ExecuteCppCode(string submittedcode, string testcode)
+        {
+            var header = "#include <bits/stdc++.h>\nusing namespace std;";
+            var tempFile = Path.GetTempFileName() + ".cpp";
+            File.WriteAllText(tempFile,header + submittedcode + testcode);
+
+            // Compile the C++ code
+            var exeFile = Path.GetTempFileName() + ".exe";
+            var e = ExecuteCode("g++", $"{tempFile} -o {exeFile}");
+
+            if (!string.IsNullOrEmpty(e)) {
+                return "Compile error:\n" + e;
+            }
+
+            // Execute the compiled C++ code
+            return ExecuteCode(exeFile, "");
+        }
+
+        private static string ExecuteCode(string command, string arguments)
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = arguments,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            process.WaitForExit();
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                return error;
+            }
+            return output;
+        }
+    }
+    private readonly AppDbContext _context;
+
+    public CodeJudgeService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<string> JudgeCodeAsync(int userId, int problemId, string submittedCode, int language)
+    {
+        var testCode = await _context.Problems
+            .Where(p => p.ProblemId == problemId)
+            .Select(p => p.TestCode)
+            .FirstOrDefaultAsync();
+
+        if (language == 1)
+        {
+            var result = CodeExecutor.ExecuteCppCode(submittedCode, testCode[language]);
+            return result;
+        }
+
+        return "";
+    }
+}
