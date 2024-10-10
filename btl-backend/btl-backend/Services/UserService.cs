6cf7ad563402f31a819926747dc68ac37f3e8739
Hong Phuc Nguyen <tomnguyen1510.4@gmail.com>
2024-10-10 20:11:22 +0700

    BTL-41 Refactor UserService and add methods for retrieving user submissions, and submissions per problem

---
+++ btl-backend/btl-backend/Services/UserService.cs
@@ -0,0 +1,74 @@
+﻿using btl_backend.Data;
+using btl_backend.Dtos;
+using btl_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace btl_backend.Services;
+
+public class UserService
+{
+    private readonly AppDbContext _context;
+
+    public UserService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<ProblemDto>> GetProblemSet(int userId)
+    {
+        var problems = await _context.Users
+            .Where(u => u.UserId == userId)
+            .Select(u => u.Class)
+            .SelectMany(c => c.Problems)
+            .AsNoTracking()
+            .GroupJoin(
+                _context.Submissions,
+                problem => problem.ProblemId,
+                submission => submission.ProblemId,
+                (problem, submissions) => new { problem, submissions }
+            )
+            .SelectMany(
+                ps => ps.submissions.DefaultIfEmpty(),
+                (ps, submission) => new ProblemDto
+                {
+                    ProblemId = ps.problem.ProblemId,
+                    Title = ps.problem.Title,
+                    Difficulty = ps.problem.Difficulty,
+                    Status = submission != null ? submission.Status : null
+                }
+            )
+            .ToListAsync();
+
+        return problems;
+    }
+
+    public Problem? GetProblem(int userId, int problemId)
+    {
+        var problem = _context.Users
+            .Where(u => u.UserId == userId)
+            .Select(u => u.Class)
+            .SelectMany(c => c.Problems)
+            .Include(p => p.Topics)
+            .AsNoTracking()
+            .FirstOrDefault(p => p.ProblemId == problemId);
+        return problem;
+    }
+
+    public async Task<List<Submission>> GetUserSubmissions(int userId)
+    {
+        var submissions = await _context.Submissions
+            .Where(s => s.UserId == userId)
+            .AsNoTracking()
+            .ToListAsync();
+        return submissions;
+    }
+
+    public async Task<List<Submission>> GetProblemSubmissions(int userId, int problemId)
+    {
+        var submissions = await _context.Submissions
+            .Where(s => s.UserId == userId && s.ProblemId == problemId)
+            .AsNoTracking()
+            .ToListAsync();
+        return submissions;
+    }
+}
