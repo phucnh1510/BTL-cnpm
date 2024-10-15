3e764d65c1533972c28cc0fdca70853b8fb21128
Hong Phuc Nguyen <tomnguyen1510.4@gmail.com>
2024-10-15 14:52:26 +0700

    Implement data Model for Discussion and Comment

---
+++ btl-backend/btl-backend/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace btl_backend.Models;
+
+public class Comment
+{
+    public int CommentId { get; set; }
+
+    public int DiscussionId { get; set; }
+
+    public int AuthorId { get; set; }
+
+    [Required]
+    [MaxLength(10000)]
+    public string? Content { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
--- btl-backend/btl-backend/Models/Discussion.cs
+++ btl-backend/btl-backend/Models/Discussion.cs
@@ -1,6 +1,20 @@
-﻿namespace btl_backend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace btl_backend.Models;
 
 public class Discussion
 {
-    
+    public int DisscussionId { get; set; }
+
+    public int AuthorId { get; set; }
+
+    [Required]
+    [MaxLength(1000)]
+    public string? Title { get; set; }
+
+    [Required]
+    [MaxLength(10000)]
+    public string? Content { get; set; }
+
+    public DateTime CreateTime { get; set; }
 }
